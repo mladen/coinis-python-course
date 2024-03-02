a236fb770f056ca1f3c04250d424c9b76f5740f8
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-03-02 16:10:55 +0100

    Adds the solutin to the 26th problem

---
+++ 1domaci/26zadatak.py
@@ -0,0 +1,5 @@
+broj_stambene_jedinice = int(
+    input("Unesite cetvorocifreni broj koji predstavlja broj stambene jedinice: ")
+)
+
+print("Stambena jedinica se nalazi na", str(broj_stambene_jedinice)[2], "spratu.")
