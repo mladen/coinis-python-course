5a22ff8777f7c0963eb55da8d22a4301eb60d8a2
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-03-09 00:21:34 +0100

    Adds the solution to the 28th problem

---
+++ 2domaci/28zadatak.py
@@ -0,0 +1,7 @@
+string = "Pa i ovo je neki string"
+target = "ng"
+
+if string.endswith(target):
+    print("Da. String se zavrsava sa targetom.")
+else:
+    print("Ne. String se ne zavrsava sa targetom.")
