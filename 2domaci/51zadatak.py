593a8602b7dde8614ac5510ce0f628304e7628f4
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-03-11 10:48:47 +0100

    Adds solutions by ChatGPT, to problems 51 and 52, for comparison purposes

--- 2domaci/51zadatak.py
+++ 2domaci/51zadatak.py
@@ -1,3 +1,27 @@
+# NOTE: Rjesenje ChatGPT-a
+# def provjeri_jaku_lozinku(lozinka):
+#     # Provjera duzine lozinke
+#     if len(lozinka) < 8:
+#         return "NO"
+
+#     # Provjera postojanja malih slova, velikih slova i cifara u lozinci
+#     ima_mala_slova = any(c.islower() for c in lozinka)
+#     ima_velika_slova = any(c.isupper() for c in lozinka)
+#     ima_cifre = any(c.isdigit() for c in lozinka)
+
+#     # Ako su ispunjeni svi uslovi, lozinka je jaka
+#     if ima_mala_slova and ima_velika_slova and ima_cifre:
+#         return "YES"
+#     else:
+#         return "NO"
+
+# # Unos lozinke od korisnika
+# lozinka = input("Unesite lozinku: ")
+
+# # Provjera da li je lozinka jaka i ispis rezultata
+# print("Da li je lozinka jaka:", provjeri_jaku_lozinku(lozinka))
+
+# NOTE: Moje rjesenje
 # lozinka = "1032asdakl23@#"
 lozinka = "32135443"
 lozinka = "3213544aA"
--- 2domaci/52zadatak.py
+++ 2domaci/52zadatak.py
@@ -1,3 +1,19 @@
+# NOTE: Rjesenje ChatGPT-a
+# def prosiri_string(a, pre, suf, num):
+#     prosireni_string = pre + a + suf  # Dodajemo prefiks i sufiks originalnom stringu jednom
+#     prosireni_string *= num  # Ponavljamo ovaj prosireni string num puta
+#     return prosireni_string
+
+# # Testiranje funkcije
+# a = 'test'
+# pre = 'pr'
+# suf = 'su'
+# num = 2
+
+# rezultat = prosiri_string(a, pre, suf, num)
+# print(rezultat)
+
+# NOTE: Moje rjesenje
 a = "test"
 pre = "pr"
 suf = "su"
