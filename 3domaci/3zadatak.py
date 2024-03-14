cd0317b98832c856c8e642fda5309d5495aa0a03
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-03-14 21:56:12 +0100

    Improves the (blueprint of) the solution to the 3rd problem

--- 3domaci/3zadatak.py
+++ 3domaci/3zadatak.py
@@ -2,26 +2,28 @@ def najduzi_testerasti_podniz(lista):
     max_duzina = 0
     trenutna_duzina = 0
 
+    maksimalni_niz = []
     trenutni_niz = []
-    privremeni_niz = []
 
-    for i in range(len(lista) - 1):
-        if lista[i] < lista[i + 1]:
-            privremeni_niz.append(lista[i])
-            trenutna_duzina += 1
+    for i in range(
+        len(lista) - 1
+    ):  # -1 jer gledamo i i i+1 (poslednji element nema i+1 sa kim bi se uporedio)
+        trenutni_niz.append(lista[i])
+        trenutna_duzina += 1
 
-            print("Privremeni niz je: ", privremeni_niz)
-        else:
+        if lista[i] >= lista[i + 1]:
             trenutna_duzina = 0
-            privremeni_niz = []
+            trenutni_niz = []
+            # resetujemo trenutnu duzinu i niz
+            # ali maksimalna duzina ostaje ista
 
         if trenutna_duzina > max_duzina:
             max_duzina = trenutna_duzina
-            trenutni_niz = privremeni_niz
-    # return max(max_duzina, trenutna_duzina)
-    return trenutni_niz
+            maksimalni_niz = trenutni_niz
+    return maksimalni_niz
 
 
 # Testiranje funkcije
-lista = [1, 2, 3, 2, 1, 2, 3, 4, 3, 2, 1]
+# lista = [1, 2, 3, 2, 1, 2, 3, 4, 3, 2, 1]
+lista = [1, 2, 3, 2, 1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 3, 4, 5, 6, 7, 1]
 print("Najduzi testerasti podniz:", najduzi_testerasti_podniz(lista))
