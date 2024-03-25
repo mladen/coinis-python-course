36e3aa4c3003e091fc14e9c1d9487df1b04557d1
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-03-25 16:57:04 +0100

    Updates the solution to problem 15 by adding the code that checks all the CC numbers from cc_info.txt file; comments out some printf statements too

--- 4domaci/15zadatak.py
+++ 4domaci/15zadatak.py
@@ -5,7 +5,7 @@ from functools import reduce
 # from functools import reduce
 
 cwd = os.getcwd()  # Get the current working directory (cwd)
-file_path = os.path.join(cwd, "4domaci", "brojevi.txt")
+file_path = os.path.join(cwd, "4domaci", "cc_info.txt")
 
 card_number = "4532 8756 1234 7678"
 
@@ -16,22 +16,20 @@ def credit_card_validation(card_number):
         return False
     if not card_number.isdigit():
         return False
-    # if int(card_number[0]) != 4:
-    #     return False
 
-    print(f"Original card number: {card_number}\n")
+    # print(f"Original card number: {card_number}\n")
 
     reversed_card_number = card_number[::-1]
     reversed_card_number_list = list(reversed_card_number)
 
-    print(f"Reversed car number list: {reversed_card_number_list}")
+    # print(f"Reversed car number list: {reversed_card_number_list}")
 
     every_second_number_doubled = [
         int(number) if index % 2 == 0 else int(number) * 2
         for index, number in enumerate(reversed_card_number_list)
     ]
 
-    print(f"Every second number doubled: {every_second_number_doubled}")
+    # print(f"Every second number doubled: {every_second_number_doubled}")
 
     every_second_number_clean = []
     for number in every_second_number_doubled:
@@ -40,9 +38,9 @@ def credit_card_validation(card_number):
         # print(number)
         every_second_number_clean.append(number)
 
-    print(f"Every second number clean: {every_second_number_clean}")
+    # print(f"Every second number clean: {every_second_number_clean}")
 
-    print(f"Added and divided by 10: {sum(every_second_number_clean)/10}")
+    # print(f"Added and divided by 10: {sum(every_second_number_clean)/10}")
 
     if sum(every_second_number_clean) / 10 % 2 == 0:
         return True
@@ -50,4 +48,13 @@ def credit_card_validation(card_number):
         return False
 
 
-print(credit_card_validation(card_number))
+# Checks one credit card
+# print(credit_card_validation(card_number))
+
+# Checks all the credit cards from file cc_info.txt
+with open(file_path, "r") as file:
+    for line in file:
+        card_number = line.strip()
+        print(
+            f"Credit card {card_number} is a valid credit card: {credit_card_validation(card_number)}"
+        )
